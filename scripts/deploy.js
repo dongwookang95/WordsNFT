299fd9456b02e56e8f3532b76d9612cd7817a1dc
dongwookang95 <54433389+dongwookang95@users.noreply.github.com>
2022-02-26 18:45:28 +0900

    adding NFT minting script

---
+++ scripts/deploy.js
@@ -0,0 +1,34 @@
+const { hexStripZeros } = require("@ethersproject/bytes")
+
+const main = async() => {
+    const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
+    const nftContract = await nftContractFactory.deploy();
+    await nftContract.deployed();
+    console.log("Contract deployed to : ", nftContract.address);
+
+
+    //call the function
+    let txn = await nftContract.makeAnEpicNFT()
+
+    // Wait for it to be mined.
+    await txn.wait()
+    console.log("Mindted NFT #1")
+        //Mint another NFT for fun
+    txn = await nftContract.makeAnEpicNFT()
+
+    // Wait for it to be mined.
+    await txn.wait()
+    console.log("Mindted NFT #2")
+};
+
+const runMain = async() => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
